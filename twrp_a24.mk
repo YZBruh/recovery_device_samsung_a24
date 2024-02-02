0673e0082f3be5f9a82345b522e707789c2dd386
YZBruh <yagizzengin73@gmail.com>
2024-02-02 12:27:23 +0300

    Try fix build error

--- twrp_a24.mk
+++ twrp_a24.mk
@@ -1,18 +1,18 @@
-# Release name
-PRODUCT_RELEASE_NAME := a24
-
 # Inherit from common AOSP config
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a24/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -22,6 +22,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_NAME := twrp_a24
 PRODUCT_DEVICE := a24
 PRODUCT_MODEL := SM-A245M
+PRODUCT_RELEASE_NAME := Samsung Galaxy A24
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
